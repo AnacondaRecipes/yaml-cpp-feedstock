98ff78f2ccad3b48c95e7b321f5615ac83260666
Scott B <Scott@TheBaileyFamily.org>
2022-03-01 09:00:57 -0700

    Initial import and update of conda-forge feedstock (#1)
    Changes:
    
    - Initial fork from conda-forge.
    - Update version from 0.6.3 to 0.7.0.
    - Move shared global settings into shared specific output.
    - Remove old patch.
    - Enable author's tests.
    - Add patches to correct DLL decorators necessary for Windows testing.

---
+++ recipe/build_shared.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Isolate the build.
+mkdir build_shared
+cd build_shared || exit 1
+
+# Generate the build files.
+cmake .. -G"Ninja" ${CMAKE_ARGS} \
+    -DBUILD_SHARED_LIBS=ON \
+    -DYAML_BUILD_SHARED_LIBS=ON \
+    -DYAML_CPP_BUILD_TESTS=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+
+# Build and install.
+ninja install || exit 1
+
+# Call author's tests.
+test/yaml-cpp-tests || exit 1
