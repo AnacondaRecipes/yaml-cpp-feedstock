47387bf5fe211e202298043d7e4737a5e4f83e91
Eric Lundby <Eric.Lundby@gmail.com>
2026-02-01 10:25:49 -0700

    Fix native build

--- recipe/bld_shared.bat
+++ recipe/bld_shared.bat
@@ -20,6 +20,8 @@ cmake .. -G"Ninja" ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_CXX_FLAGS="/bigobj" ^
+    -DCMAKE_C_FLAGS="/bigobj" ^
     -DYAML_CPP_INSTALL=ON
 
 if errorlevel 1 exit 1
